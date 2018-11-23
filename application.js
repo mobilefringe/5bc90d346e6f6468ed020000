adeb3bc8b11915cb02c0d49409f231569b85a985
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-23 13:29:01 -0500

    Westbrook Mall 2.0 1.0  /application.js

--- application.js
+++ application.js
@@ -242,8 +242,19 @@ function renderHomeHours(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
     item_list.push(collection);    
     $.each( item_list , function( key, val ) {
-        var day = moment(val.day_of_week).format("ddd");
-        val.day = day;
+        if(val.day_of_week){
+            var day = moment(val.day_of_week).format('ddd');//getDay(val.day_of_week);
+            val.day = day;
+        }
+        else if(!val.day && val.is_holiday){
+            console.log("its a holiday");
+            var day = moment(val.holiday_date).format('ddd');
+            val.day = day;
+        }
+        else {
+            var day = moment().format('ddd');
+            val.day = day;
+        }
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
