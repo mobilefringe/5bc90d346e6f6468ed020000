828eb1efb03c1a2a1769530acbb40ecd3bfe7a3c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-02-01 16:15:31 -0500

    Westbrook Mall 2.0 1.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,21 +1,7 @@
 function init(e){
     $('<div class="modal-backdrop custom_backdrop_load"><div class="loader">Loading...</div></div>').appendTo(document.body);
-    
-    // //Using i18n for localization, for more info please visit http://i18next.com/
-    // i18n.init({preload: [getStorage().primary_locale,getStorage().secondary_locale],resGetPath: '../__lng__.json',fallbackLng: false }, function(t) {
-    //     var current_locale = "";
-    //     if(typeof(Cookies.get('current_locale')) != 'undefined' ){
-    //         current_locale = Cookies.get('current_locale')
-    //     }
-    //     if(current_locale == Cookies.get('primary_locale')){
-    //         setPrimaryLanguage();
-    //     }else{
-    //         setSecondaryLanguage();
-    //     }
-    // });
-    
+
     // If there is no language set it to the primary locale.
-    // log(Cookies.get('current_locale'))
     if (!Cookies.get('current_locale')) {
         setPrimaryLanguage();
     }
